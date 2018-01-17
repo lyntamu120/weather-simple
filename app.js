b5aff817c2f53853988520908cc20f3c28e2e9ea
王希宁 <xiningwang120@gmail.com>
2018-01-16 20:06:55 -0600

    chain location and weather callbacks together

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const request = require('request');
 const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js')
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -16,22 +16,17 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-console.log(argv);
-var encodedAddress = encodeURIComponent(argv.a);
-request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google Servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OK'){
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-        console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+geocode.fetchLocation(argv.a, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log("Can't do the query");
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, results) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`It is ${results.realTemp}, but it feels like ${results.temp}`);
+            }
+        });
     }
-
 });
